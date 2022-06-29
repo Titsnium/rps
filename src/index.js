a0e732ebe2131fbaeeb948a130c979f56e5bf707
Titsnium <titsniumoaknium@gmail.com>
2022-06-29 18:55:06 +0800

    added some comments

--- src/index.js
+++ src/index.js
@@ -14,15 +14,18 @@ document.getElementById("Scissors").onclick = function() {
     game();
 }
 
-
+// The scores
 window.playerScore = 0;
 window.computerScore = 0;
+
 // The main game function
 function game() {
-
+    // The choices for the computer 
     var computerMoves = ["Rock", "Paper", "Scissors"];
+    // Picks a random option from the array above
     var computerMove = computerMoves[Math.floor(Math.random()*computerMoves.length)];
     
+
     if (
     playerMove == "Rock" && computerMove == "Paper" || 
     playerMove == "Paper" && computerMove == "Scissors" || 
@@ -43,8 +46,11 @@ function game() {
     else if (playerMove == computerMove) {
         console.log("DRAW!!!");
     }
+    // Displays the current scores on the html page
     document.getElementById("playerScore").innerHTML = "PLAYER: " + window.playerScore;
     document.getElementById("computerScore").innerHTML = "COMPUTER: " + window.computerScore;
+
+    // Checks to see if the game is basically finished and displays the result on the html page
     if (window.playerScore >= 5) {
         document.getElementById("resultText").innerHTML = "You WIN!";
         playerScore = 0;
@@ -54,6 +60,6 @@ function game() {
         playerScore = 0;
         computerScore = 0;
     } else {
-        document.getElementById("resultText").innerHTML = "";
+        document.getElementById("resultText").innerHTML = "GAME!";
     }
 }
