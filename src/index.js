2be9b7d63196f0fbacdcc9c43a2bb4fdb908a2b0
Titsnium <titsniumoaknium@gmail.com>
2022-06-29 14:21:14 +0800

    finally added js, just checking which buttons were clicked

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
+// Checks to see if any of these buttons were clicked, and sets the playerMove variable accordingly
+document.getElementById("Rock").onclick = function() {
+    window.playerMove = "Rock";
+    game();
+}
 
+document.getElementById("Paper").onclick = function() {
+    window.playerMove = "Paper";
+    game();
+}
 
+document.getElementById("Scissors").onclick = function() {
+    window.playerMove = "Scissors";
+    game();
+}
+
+// The main game function
+function game() {
+    if (window.playerMove === "Rock") {
+        console.log("You played ROCK!!!");
+    } else if (window.playerMove === "Paper") {
+        console.log("You played Paper!!!");
+    } else {
+        console.log("You played SCISSORS!!!");
+    }
+}
