8cfa22d5b0ac1e10c0e8beda7a316e3cd44e9ead
Titsnium <titsniumoaknium@gmail.com>
2022-06-29 18:47:35 +0800

    finished the js logic on the game

--- src/index.js
+++ src/index.js
@@ -14,13 +14,46 @@ document.getElementById("Scissors").onclick = function() {
     game();
 }
 
+
+window.playerScore = 0;
+window.computerScore = 0;
 // The main game function
 function game() {
-    if (window.playerMove === "Rock") {
-        console.log("You played ROCK!!!");
-    } else if (window.playerMove === "Paper") {
-        console.log("You played Paper!!!");
+
+    var computerMoves = ["Rock", "Paper", "Scissors"];
+    var computerMove = computerMoves[Math.floor(Math.random()*computerMoves.length)];
+    
+    if (
+    playerMove == "Rock" && computerMove == "Paper" || 
+    playerMove == "Paper" && computerMove == "Scissors" || 
+    playerMove == "Scissors" && computerMove == "Rock"
+    ) {
+        console.log("YOU WIN!!!");
+        window.playerScore += 1;
+    } 
+    
+    else if (
+    computerMove == "Rock" && playerMove == "Paper" ||
+    computerMove == "Paper" && playerMove == "Scissors" || 
+    computerMove == "Scissors" && playerMove == "Rock") {
+        console.log("YOU LOSE!!!");
+        window.computerScore += 1 ;
+    } 
+    
+    else if (playerMove == computerMove) {
+        console.log("DRAW!!!");
+    }
+    document.getElementById("playerScore").innerHTML = "PLAYER: " + window.playerScore;
+    document.getElementById("computerScore").innerHTML = "COMPUTER: " + window.computerScore;
+    if (window.playerScore >= 5) {
+        document.getElementById("resultText").innerHTML = "You WIN!";
+        playerScore = 0;
+        computerScore = 0;
+    } else if (window.computerScore >= 5) {
+        document.getElementById("resultText").innerHTML = "You LOSE!";
+        playerScore = 0;
+        computerScore = 0;
     } else {
-        console.log("You played SCISSORS!!!");
+        document.getElementById("resultText").innerHTML = "";
     }
 }
